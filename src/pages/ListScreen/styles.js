5610067be4920e6fb6fc4c9eac9e98b8b5160cbe
andre.silva <andre.silva@marimex.com.br>
2024-06-17 11:41:31 -0300

    Configurando o Redux e o React Navigation

---
+++ src/pages/ListScreen/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+
+`;
+
+export const Texto = styled.Text`
+
+`;
+
+export const Botao = styled.Button`
+
+`;
---
+++ src/stacks/MainStack.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import EditNoteScreen from "../pages/EditNoteScreen";
+import ListScreen from "../pages/ListScreen";
+
+
+
+const MainStack = createStackNavigator();
+
+export default () => {
+     return (
+        <MainStack.Navigator>
+        <MainStack.Screen name="List" component={ListScreen}/>
+        <MainStack.Screen name="EditNote" component={EditNoteScreen}/>
+    </MainStack.Navigator>
+     );
+}
